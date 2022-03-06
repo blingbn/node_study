e08f8e8249f249ef53bf47f9df49ce4978023535
Bitna Lee <erin@ERIN.local>
2022-03-06 22:37:12 +0900

    study2 basic of node

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-// 미들웨어
-function testMiddleware(req, res, next) {
+function testMiddleWare(req, res, next) {
   console.log("첫번째 미들웨어");
   next();
 }
-function testMiddleware2(req, res, next) {
+
+function testMiddleWare2(req, res, next) {
   console.log("두번째 미들웨어");
   next();
 }
 
-router.get("/", testMiddleware, testMiddleware2, (req, res) => {
-  res.send("admin 미들웨어");
+router.get("/", testMiddleWare, testMiddleWare2, (req, res) => {
+  res.send("admin app");
 });
 
-router.get("/products", (req, res) => {
-  // res.send("admin products");
-  res.render("admin/products.html", {
-    message: `<h1>태그가 출력됩니다.</h1>`,
-    online: "express",
-  });
+router.get("/products", (_, res) => {
+  res.render(
+    "admin/products.html",
+    { message: "hello" } // message 란 변수를 템플릿으로 내보낸다.
+  );
 });
 
-router.get("/products/write", (req, res) => {
+router.get("/products/write", (_, res) => {
   res.render("admin/write.html");
 });
 
